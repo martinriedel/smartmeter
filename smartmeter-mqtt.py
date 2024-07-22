c1611d6e855a7c42c7621c1a4d0a11f8bf2cd6a8
Martin Riedel <martin.riedel@zeiss.com>
2024-07-22 11:17:19 +0200

    Update smartmeter-mqtt.py

--- smartmeter-mqtt.py
+++ smartmeter-mqtt.py
@@ -94,10 +94,10 @@ class SmartMeterThread:
                         elif val['objName'] == b'\x01\x00\x10\x07\x00\xff':
                             self.power_W = pow10(val['value'], val['scaler'])
 
-                data = {'totalin': '{:.4f}'.format( self.energy_consumption_Wh / 1000.0),
-                        'totalout': '{:.4f}'.format(self.energy_supply_Wh / 1000.0), 'power': self.power_W}
-                mqtt_client.publish("devices/smartmeter/import", json.dumps(self.energy_consumption_Wh))
-                mqtt_client.publish("devices/smartmeter/export", json.dumps(self.energy_supply_Wh))
+                data = {'totalin': '{:.4f}'.format(self.energy_consumption_Wh / 1000.0),
+                        'totalout': '{:.4f}'.format(self.energy_supply_Wh / 1000.0), 'powercur': self.power_W}
+                mqtt_client.publish("devices/smartmeter/import", json.dumps('{:.4f}'.format(self.energy_consumption_Wh / 1000.0)))
+                mqtt_client.publish("devices/smartmeter/export", json.dumps('{:.4f}'.format(self.energy_supply_Wh / 1000.0)))
                 mqtt_client.publish("devices/smartmeter/power", json.dumps(self.power_W))
                 mqtt_client.publish("devices/smartmeter/data", json.dumps(data))
                 # print('Bezug = {:.1f} Wh  Lieferung = {:.1f} Wh  Leistung = {:4d} W'.format(energy_consumption_Wh, energy_supply_Wh, power_W))
